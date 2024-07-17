c462c76cbfc75eb69f6c23fc2ffa442eedf7efa6
QuentinVigier <vigierquentinpro@gmail.com>
2024-07-17 12:34:03 +0200

    Correction redirection form

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { IoSearchOutline } from "react-icons/io5"
+import { NavLink } from "react-router-dom"
 
 
 const Hero = () => {
@@ -9,6 +10,7 @@ const Hero = () => {
             <span className="text-orange-300"> cuisine du monde</span></h1>
 
             <form action="/search" className="relative flex items-center p-4 bg-white rounded">
+            <NavLink to="/search" className="absolute inset-y-0 left-0 flex items-center pl-3"></NavLink>
             <IoSearchOutline className="w-5 h-5 mr-2 text-neutral-400"/>
             <input className="outline-none w-full placeholder:text-[#1b2629]" name="query" type="search" placeholder="Cherche une recette" id="search" required />
             </form>
