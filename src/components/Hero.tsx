ba067d406fb002c30d84e602aaadf54133521ebc
QuentinVigier <vigierquentinpro@gmail.com>
2024-07-17 12:51:37 +0200

    Correction redirection formulaire

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,22 @@
 import { IoSearchOutline } from "react-icons/io5"
-import { NavLink } from "react-router-dom"
-
+import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
+  
+  const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate('/search?query=' + event.currentTarget.query.value);
+    
+  };
+  
   return (
     <div className="px-5 mb-10 xl:px-10 md:w-1/2">
         <h1 className="mt-6 mb-10 text-5xl xl:text-6xl text-center font-bold text-[#2A3342] leading-normal xl:leading-relaxed">
             Un blog fait pour les amoureux de la 
             <span className="text-orange-300"> cuisine du monde</span></h1>
 
-            <form action="/search" className="relative flex items-center p-4 bg-white rounded">
-            <NavLink to="/search" className="absolute inset-y-0 left-0 flex items-center pl-3"></NavLink>
+            <form className="relative flex items-center p-4 bg-white rounded" onSubmit={handleSubmit}>
             <IoSearchOutline className="w-5 h-5 mr-2 text-neutral-400"/>
             <input className="outline-none w-full placeholder:text-[#1b2629]" name="query" type="search" placeholder="Cherche une recette" id="search" required />
             </form>
